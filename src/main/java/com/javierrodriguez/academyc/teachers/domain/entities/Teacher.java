5a92342a44edde0082cc005edef78629e9548beb
Xavi <xavi@geekshubs.com>
2020-11-09 11:47:10 +0100

    Create connection to DB

---
+++ src/main/java/com/javierrodriguez/academyc/teachers/domain/entities/Teacher.java
@@ -0,0 +1,13 @@
+package com.javierrodriguez.academyc.teachers.domain.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import java.io.Serializable;
+
+@Entity
+@Table(name="teachers")
+public class Teacher implements Serializable {
+
+
+
+}
