a1e2f72b7438065f4c58cd6be76ee0cc76897da1
Xavi <xavi@geekshubs.com>
2020-11-09 16:00:32 +0100

    test to route post

--- src/test/java/com/javierrodriguez/academyc/teachers/ui/TeacherControllerTest.java
+++ src/test/java/com/javierrodriguez/academyc/teachers/ui/TeacherControllerTest.java
@@ -1,6 +1,9 @@
 package com.javierrodriguez.academyc.teachers.ui;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import com.javierrodriguez.academyc.AcademycApplication;
+import com.javierrodriguez.academyc.teachers.domain.entities.Teacher;
 import org.aspectj.lang.annotation.Before;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,8 +14,13 @@ import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
+
+import java.util.UUID;
+
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
@@ -40,7 +48,20 @@ public class TeacherControllerTest {
                   .andExpect(status().isOk());
       }
 
+      @Test
+     public void CreateTeacher() throws Exception{
+             Teacher teacher = new Teacher(UUID.randomUUID(), "prueba","prueba");
+             mvc.perform(post("/teachers").contentType(MediaType.APPLICATION_JSON).content(this.toJson(teacher)))
+                     .andDo(print())
+                     .andExpect(status().isOk())
+                     .andExpect(jsonPath("$.name").value(teacher.getName()));
+     }
 
+    static byte[] toJson(Object object) throws Exception
+    {
+        Gson gson = new GsonBuilder().setDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSZ").create();
 
+        return gson.toJson(object).getBytes();
+    }
 
 }
