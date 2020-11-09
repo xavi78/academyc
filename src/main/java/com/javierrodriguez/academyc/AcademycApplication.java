e3ff883d8e31239b3215a6d793a99ddae48cb593
Xavi <xavi@geekshubs.com>
2020-11-09 10:13:06 +0100

    first commit

---
+++ src/main/java/com/javierrodriguez/academyc/AcademycApplication.java
@@ -0,0 +1,13 @@
+package com.javierrodriguez.academyc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcademycApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AcademycApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/javierrodriguez/academyc/AcademycApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javierrodriguez.academyc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcademycApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
