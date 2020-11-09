5f763d54330a27baa2945a98870742d2a8c5999b
Xavi <xavi@geekshubs.com>
2020-11-09 11:01:28 +0100

    Init test environment

--- src/test/java/com/javierrodriguez/academyc/AcademycApplicationTests.java
+++ src/test/java/com/javierrodriguez/academyc/AcademycApplicationTests.java
@@ -3,7 +3,7 @@ package com.javierrodriguez.academyc;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes= AcademycApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class AcademycApplicationTests {
 
     @Test
